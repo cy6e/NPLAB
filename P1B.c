1860a1399765592a2a62d83f4ca5bec7fdd1b5a4
deekshaakumar <130745979+deekshaakumar@users.noreply.github.com>
2023-04-16 14:13:20 +0530

    Add files via upload

---
+++ P1B.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+void sender()
+int main(void)
+{
+void sender();
+{
+int i,j,n;
+int count=0;zero=0,zerocounter=0;
+int result[i],msg[i];
+printf("ent the msg length");
+scanf("%d",&n);
+printf("ent the message");
+for(i=0;i<n;i++)
+{
+scanf("%d",a[i]);
+if(i==0)
+result[i]=msg[i];
+i++;
+zerocounter+=1;
+else (i=1)
+result[i]=msg[i];
+i++;
+count++;
+}
+result[0]=0;
+result[1]=1;
+result[2]=1;
+result[3]=1;
+result[4]=1;
+result[5]=1;
+result[6]=1;
+result[7]=0;
+j=8;
+if((count==5)&&zero=0)
+{
+for(i=0;i<n;i++)
+result[i]=0;
+if(count<5)
+{
+i++;
+count=0;
+}
+else
+if(count=0)
+{
+result[i]=msg[i];
+i++;
+count++;
+}
+result[j++]=0;
+result[j++]=1;
+result[j++]=1;
+result[j++]=1;
+result[j++]=1;
+result[j++]=1;
+result[j++]=1;
+result[j++]=0;
+printf("\n");
+printf("\n the bit stuffed message is");
+for(i=0;i<n;i++)
+{
+printf("%d",result[i]);
+}
+for(i=0;i<n;i++)
+printf("unstuffed data is %d",msg[i]);
+}
+}
